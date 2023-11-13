cfb1e13cb97f0ecc08e2c3712b0cb3135a7b3910
Alumno-2026415 <143697133+Alumno-2026415@users.noreply.github.com>
2023-11-12 23:07:43 -0600

    Create Pía pseucódigo.py
    Aquí está el pseudocódigo

---
+++ Pía pseucódigo.py
@@ -0,0 +1,90 @@
+Inicio
+Repetir hasta que el usuario elija salir:
+    Mostrar menú principal
+    Leer opción_principal
+
+    Si opción_principal es 1:
+        Repetir hasta que el usuario elija volver al menú principal:
+            Mostrar menú de notas
+            Leer opción_notas
+
+            Si opción_notas es 1:
+                Registrar una nota
+            Si opción_notas es 2:
+                Cancelar una nota
+            Si opción_notas es 3:
+                Recuperar una nota
+            Si opción_notas es 4:
+                Consultar por período
+            Si opción_notas es 5:
+                Salir del menú de notas
+
+    Si opción_principal es 2:
+        Repetir hasta que el usuario elija volver al menú principal:
+            Mostrar menú de clientes
+            Leer opción_clientes
+
+            Si opción_clientes es 1:
+                Agregar un cliente
+            Si opción_clientes es 2:
+                Suspender un cliente
+            Si opción_clientes es 3:
+                Recuperar un cliente
+            Si opción_clientes es 4:
+                Mostrar menú de consultas y reportes de clientes
+                Leer opción_reportes_clientes
+
+                Si opción_reportes_clientes es 1:
+                    Listado de clientes registrados ordenado por clave
+                Si opción_reportes_clientes es 2:
+                    Listado de clientes registrados ordenado por nombre
+                Si opción_reportes_clientes es 3:
+                    Volver al menú anterior
+            Si opción_clientes es 5:
+                Volver al menú principal
+
+    Si opción_principal es 3:
+        Repetir hasta que el usuario elija volver al menú principal:
+            Mostrar menú de servicios
+            Leer opción_servicios
+
+            Si opción_servicios es 1:
+                Agregar un servicio
+            Si opción_servicios es 2:
+                Suspender un servicio
+            Si opción_servicios es 3:
+                Recuperar un servicio
+            Si opción_servicios es 4:
+                Mostrar menú de consultas y reportes de servicios
+                Leer opción_reportes_servicios
+
+                Si opción_reportes_servicios es 1:
+                    Búsqueda por clave de servicio
+                Si opción_reportes_servicios es 2:
+                    Búsqueda por nombre de servicio
+                Si opción_reportes_servicios es 3:
+                    Listado de servicios ordenado por clave
+                Si opción_reportes_servicios es 4:
+                    Listado de servicios ordenado por nombre
+                Si opción_reportes_servicios es 5:
+                    Volver al menú anterior
+            Si opción_servicios es 5:
+                Volver al menú principal
+
+    Si opción_principal es 4:
+        Repetir hasta que el usuario elija volver al menú principal:
+            Mostrar menú de estadísticas
+            Leer opción_estadisticas
+
+            Si opción_estadisticas es 1:
+                Estadísticas de servicios más prestados
+            Si opción_estadisticas es 2:
+                Estadísticas de clientes con más notas
+            Si opción_estadisticas es 3:
+                Estadísticas de promedio de montos de notas
+            Si opción_estadisticas es 4:
+                Volver al menú principal
+
+    Si opción_principal es 5:
+        Salir del programa
+Fin
